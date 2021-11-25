b8222465ce78b7b6b1832f931390ec534ed74b2c
kvammee <elliott.kvamme@gmail.com>
2021-11-24 22:42:35 -0600

    turned off sync force....

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ app.use(session(sess));
 app.use(routes);
 
 // turn on connection to db and server
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
