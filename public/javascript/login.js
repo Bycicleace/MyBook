f0521862406a4162103f905ac7b6bbbdbf51702f
Raymond Cerney <cerne004@gmail.com>
2021-11-30 20:18:34 -0600

    added signup handler to login.js

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -21,3 +21,33 @@ async function loginFormHandler(event) {
     }
   }
 }
+
+async function signupFormHandler(event) {
+  event.preventDefault();
+
+  const firstName = document.querySelector('#firstname-signup').value.trim(); 
+  const lastName = document.querySelector('#lastname-signup').value.trim(); 
+  const penName = document.querySelector('#penname-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (penName && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'post',
+      body: JSON.stringify({
+        firstName,
+        lastName,
+        penName,
+        email,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
