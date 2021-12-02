9978ba821038e40eefdc4ae78320961044b10923
Raymond Cerney <cerne004@gmail.com>
2021-12-01 20:20:28 -0600

    fixed stuff like adding posts and stories

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,14 +1,15 @@
 async function newFormHandler(event) {
   event.preventDefault();
 
-  const title = document.querySelector('input[name="post-title"]').value;
-  const post_url = document.querySelector('input[name="post-url"]').value;
+  const content = document.querySelector('.post-content').value.trim();
+  const id = window.location.toString().split('/')[window.location.toString().split('/').length - 1];
 
   const response = await fetch(`/api/posts`, {
     method: 'POST',
     body: JSON.stringify({
-      title,
-      post_url
+      content: content,
+      user_id: req.session.user_id,
+      story_id: id
     }),
     headers: {
       'Content-Type': 'application/json'
--- public/javascript/add-story.js
+++ public/javascript/add-story.js
@@ -1,25 +1,44 @@
-async function newFormHandler(event) {
+async function newStoryFormHandler(event) {
   event.preventDefault();
 
-  const title = document.querySelector('input[name="post-title"]').value;
-  const post_url = document.querySelector('input[name="post-url"]').value;
+  const title = document.querySelector('.new-title').value;
+  const content = document.querySelector('.new-post').value;
+  const story_id;
 
-  const response = await fetch(`/api/posts`, {
+  const response = await fetch(`/api/stories`, {
     method: 'POST',
     body: JSON.stringify({
-      title,
-      post_url
+      title: title,
+      user_id: req.session.user_id
     }),
     headers: {
       'Content-Type': 'application/json'
     }
+  }).then(dbStoriesData => {
+   story_id = dbStoriesData.id;
   });
 
   if (response.ok) {
-    document.location.replace('/dashboard');
+    const secondResponse = await fetch('/api/posts', { 
+      method: 'POST',
+      body: JSON.stringify({
+        content: content,
+        user_id: req.session.user_id,
+        story_id: story_id
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
   } else {
     alert(response.statusText);
   }
 }
 
-document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
+document.querySelector('.create-story').addEventListener('submit', newStoryFormHandler);
